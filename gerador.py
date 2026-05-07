2eb931ede62d121c24c6a109b96e99f640c7cd22
Umohau Haraba <xiricoaviva@gmail.com>
2026-05-07 23:42:27 +0200

    feat(gerador): adiciona 3 novos metodos
    -adiciona o metodo gerar pedido_licenca
    
    -adicionao metodo gerar_pedido_certif_sanitario
    
    -adiciona o metodo gerar_pedido_declar_noncites
    
    -adiciona o metodo gerar_pedido_certif_origem
    
    -adiciona o metodo gerar_pedido_licenca

---
+++ gerador.py
@@ -0,0 +1,225 @@
+from pathlib import Path
+import os
+from infra import InfraGerador
+from docxtpl import DocxTemplate
+from infra import logger
+
+
+
+class Documento:
+    def __init__(self):
+        pass
+        
+    def carregar_modelo(self, caminho:str) ->DocxTemplate:
+        """
+        Verifica se o arquivo existe no caminho fornecido no init.
+        Args:
+            caminho(str): caminho do arquivo.
+            
+        Returns:
+            DocxTemplante: uma instancia de DocxTemplante do modelo.
+            
+        Raises:
+            FileNotFoundError: se o arquivo nao for encontrdo.
+        """
+       
+   
+        #verificando se o arquivo existe
+        if not Path(caminho).exists():
+            logger.error("erro ao caregar modelo.modelo nao encontrado em '%s'", caminho)
+            raise FileNotFoundError(f"modelo nao encontrado em {caminho}")
+            
+            
+        #tentando instaciar DocxTemplate  com o 
+        #caminho se o aquivo existir
+        try: 
+            return DocxTemplate(caminho)
+        except Exception as e:
+            logger.error("erro inesperado ao caregar modelo em '%s' .\n erro: %s",caminho, exc_info=True)
+            raise 
+    
+    
+        
+    def preencher(self,modelo: DocxTemplate, dados:dict) -> object:
+        """
+        Preeche os dados do docionario dados, nos templantes word no modelo.
+        
+        Args:
+            modelo(DocxTemplate): modelo a ser preencido
+            dados(dict): dicionario de dados preenchidos no formato {campo: dado}.
+        
+        Returns:
+            DocxTemplate: um objeto de documento word preenchido
+        """
+        
+        #preenchendo o modelo e retornando
+        #o documento preenchido
+        modelo.render(dados)
+        return modelo
+        
+       
+    def salvar(self, arquivo:object, caminho:str) -> bool:
+        """
+        Guarda o arquivo no disco.
+        
+        Args:
+            arquivo(object): um arquivo que possa ser persistido (que possua o metodo save()).
+            caminho(str): diretorio com onome do arquivo.
+        Returns:
+            True(bool):se o arquivo foi salvo
+            
+        Raises:
+            OSError: se o disco estiver cheio.
+            PermissionError: se nao tiver permissao.
+            IOError: se nao for possivel escrever no disco.
+        """
+        try:
+            arquivo.save(caminho)
+            return True
+        except PermissionError:
+            logger.warning("erro de permissao ao salvar arquivo em %s", caminho)
+            raise
+        except OSError:
+            logger.warning("erro ao salvar arquivo disco cheio")
+            raise
+        except IOError:
+             logger.error("erro ao salvar arquivo, nao foi possiver escrever no disco: %s", caminho)
+             raise
+        except Exception as e:
+            logger.error("ocorreu um erro inesperado ao salvar arquivo em %s", caminho , exc_info=True)
+            raise
+        
+        
+    def abrir(self, caminho: str) -> None:
+        """
+        Abre o arquivo no caminho fornecido.
+        refactor(Documento): remove o init na classe Documento 
+        Args:
+            caminho(str): caminho do arquivo a abrir.
+            
+        Returns:
+            None
+            
+        Raises:
+            FileNotFoundError: se o arquivo nao for encontrado
+            PermissionError: se nao tiver permissao
+        """
+        try:
+            os.startfile(caminho)
+        except (FileNotFoundError, PermissionError) as e:
+            logger.error("falha ao abrir arquivo em %s\nERRO: %s", caminho, e.strerror)
+            raise
+        
+        
+class Gerador:
+    def __init__(self):
+        self._documento=Documento()
+        self._base=InfraGerador().base
+        self._caminho_gerado=self._base/"documentos gerados"
+        
+        
+    def gerar_recibo(self, dados: dict) -> Path:
+        nome=f"recibo{dados.get('data')}.docx" #nome dos recibos gerados
+        caminho_modelo=self._base/'modelos'/"recibo_template.docx"
+        caminho_novo_recibo= self._caminho_gerado/"recibos"/nome
+        
+        #carregando modelo
+        modelo_rec=self._documento.carregar_modelo(str(caminho_modelo))
+        #preenchendo modelo
+        documento_gerado=self._documento.preencher(modelo_rec, dados)
+        #salvando novo recibo
+        self._documento.salvar(documento_gerado, str(caminho_novo_recibo))
+        return caminho_novo_recibo
+        
+
+    def gerar_pedido_quota(self, dados:dict) -> Path:
+        nome=f"ped_quota{dados.get('data')}"
+        caminho_modelo=str(self._base/modelos/"ped_quota_template.docx")
+        caminho_nova_quota=self._caminho_gerado/"pedido_quota"/nome
+        
+        #carrega modelo para preechimento
+        modelo_quota= self._documento.carregar_modelo(caminho_modelo)
+        
+        #preenche modelo
+        novo_pedido_quota= self._documento.preencher(modelo_quota, dados)
+        
+        #salva o documento gerado
+        self._documento.salvar(novo_pedido_quota, str(caminho_nova_quota))
+        return caminho_nova_quota
+       
+       
+    def gerar_pedido_licenca(self, dados:dict) ->Path:
+        nome=f"pedido_licenca_{dados.get('data')}.docx"
+        caminho_modelo= str(self._base/modelos/"pedido_licenca_template.docx")
+        caminho_novo_pedido= self._base/self._caminho_gerado/"pedidos_licenca"/nome
+        
+        #carrega modelo para preenchimento
+        modelo=self._documento.carregar_modelo(caminho_modelo)
+        
+        #preenche o modelo
+        documento_gerado= self._documento.preencher(modelo, dados)
+        
+        #salva o documento gerado
+        self.documenro.salvar(documento_gerado,str( caminho_novo_pedido))
+        return  caminho_novo_pedido
+        
+    def gerar_pedido_certif_origem(self, dados:dict) ->Path:
+        nome=f"pedido_certf_origem_{dados.get('data')}.docx"
+        caminho_modelo= str(self._base/modelos/"pedido_certf_origem_template.docx")
+        caminho_novo_pedido= self._base/self._caminho_gerado/"certificados_origem_P"/nome
+        
+        #carrega modelo para preenchimento
+        modelo=self._documento.carregar_modelo(caminho_modelo)
+        
+        #preenche o modelo
+        documento_gerado= self._documento.preencher(modelo, dados)
+        
+        #salva o documento gerado
+        self.documenro.salvar(documento_gerado,str( caminho_novo_pedido))
+        return  caminho_novo_pedid
+        
+        
+    def gerar_pedido_declar_noncites(self, dados:dict) ->Path:
+        nome=f"pedido_decla_noncites_{dados.get('data')}.docx"
+        caminho_modelo= str(self._base/modelos/"pedido_decla_noncites_template.docx")
+        caminho_novo_pedido= self._base/self._caminho_gerado/"declaracoes_non_cites_P"/nome
+        
+        #carrega modelo para preenchimento
+        modelo=self._documento.carregar_modelo(caminho_modelo)
+        
+        #preenche o modelo
+        documento_gerado= self._documento.preencher(modelo, dados)
+        
+        #salva o documento gerado
+        self.documenro.salvar(documento_gerado,str( caminho_novo_pedido))
+        return  caminho_novo_pedido
+    
+    def gerar_pedido_certif_sanitario(self, dados:dict) ->Path:
+        nome=f"pedido_certif_sanitario_{dados.get('data')}.docx"
+        caminho_modelo= str(self._base/modelos/"pedido_certif_sanitario_template.docx")
+        caminho_novo_pedido= self._base/self._caminho_gerado/"certificados_sanitarios_P"/nome
+        
+        #carrega modelo para preenchimento
+        modelo=self._documento.carregar_modelo(caminho_modelo)
+        
+        #preenche o modelo
+        documento_gerado= self._documento.preencher(modelo, dados)
+        
+        #salva o documento gerado
+        self.documenro.salvar(documento_gerado,str( caminho_novo_pedido))
+        return  caminho_novo_pedido
+        
+g=Gerador()
+dados={
+    'nome':'joao',
+    'endereco':'yuen po street birds garden kwoloon hong kong',
+    'quantidade' : 1200,
+    'caixas':8,
+    'quantidade_caixa':120,
+    'peso':92,
+    'dimencoes': '20x92x71',
+    'preco':'15' ,
+    'preco_tot':15000,
+    'data':'2000-62'
+        }
+g.gerar_recibo(dados)
