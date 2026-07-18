0529a0389190c03b70a9289c3464ef72d00f2917
Umohau <muhauharaba33@gmail.com>
2026-07-18 16:53:14 +0200

    feat(messageBox_repository): adiciona metodos de busca e delete e update - delete_by_status - update - _get - get_by_status - get_by_id - get_by_channel

--- repositories/messageBox_repository.py
+++ repositories/messageBox_repository.py
@@ -17,4 +17,34 @@ Ensure that the same Connector object is used in both the InfraData and the Oper
         with self.engine.begin() as conexao:
              res=conexao.execute(add)
              return res.inserted_primary_key[0]
-             
+
+
+    def delete_by_status(status:str) -> int:
+        delete= sa.delete().where(self.tabela.c.status == status)
+        with self.engine.begin() as conexao:
+            res=conexao.execute(delete)
+            return re.rowcount
+            
+            
+    def update(dados:dict, id:int) -> int:
+        update= sa.update().values(dados).where(self.tabela.id== id)
+        with self.engine.begin() as conexao:
+            res= conexao.execute(update)
+
+    def _get(coluna: str, filtro: optional[str]):
+        stm= self.tabela.select().where(self.tabela.c[coluna]==filtro)
+        with self.engine.begin() as conexao:
+            res= conexao.execute(stm).fetchall()
+            return [dict(messege) for  messege in res]
+        
+
+    def get_by_status(status: str) -> list[dict]:
+        return self._get(coluna= 'status', filtro= status)
+            
+            
+    def get_by_id(id: int) -> dict:
+            return self._get(coluna= "id", filtro=id)[0]
+
+
+    def get_by_channel(channel: str) -> list[dict]:
+            return self._get(coluna= "channel", filtro= channel)
