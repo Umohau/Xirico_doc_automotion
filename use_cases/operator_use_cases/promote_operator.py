c4ac2cf04f47a88c8d4200fd244c9c46e2e90734
Umohau <muhauharaba33@gmail.com>
2026-07-22 23:38:51 +0200

    feat(promote_operator): cria o use_case disable_operator

---
+++ use_cases/operator_use_cases/promote_operator.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from Projeto_xirico.exc import PermissionDeniedError
+import logging
+
+
+if TYPE_CHECKING:
+    from Projeto_xirico.repositories.operator_repository import OperatorRepository
+    from Projeto_xirico.repositories.messageBox_repository import messageBoxRepository
+    from Projeto_xirico.segurança import Auditoria
+    from Projeto_xirico.profile import Profile
+    
+class PromoteOperator:
+    def __init__(
+        self, 
+        message_box: messageBoxRepository,
+        repo: OperatorRepository,
+        profile: Profile,
+        audit: Auditoria
+        ) :
+        self._message_box= message_box
+        self._repo= repo
+        self._profile= profile
+        self._audit= audit
+            
+            
+    def execute(self, id: int) -> int:
+        #verifica se o operdor em sessao é ADM
+        if not self._profile.ADM:
+            raise PermissionDeniedError('nao ADMs nao podem promover operadores')
+        operador= repo.search_id(id)# revupera os dados do operador a promover
+        affect= self._repo.update(
+            dados={"ADM": True})# executa a promocao
+        #audita a accao
+        self._audit.auditar(
+            operador= id,
+            operacao= "Promote_operator",
+            detalhes= f"promoveu a ADM o operador de id: {id}")
+        #adiciona uma mensagem a caixa de mensagens para envio
+        self._message_box.add_(
+            dados={
+                "to":operador.get("email"),
+                "type":'promote',
+                "name": operador.get("nome"),
+                "channel": 'email'
+               } )
+        return effect
