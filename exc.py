9b8b17f4912e861bfd46f66dca794cc2ae133967
Umohau <muhauharaba33@gmail.com>
2026-07-29 13:50:00 +0200

    feat(exc):cria o modulo exc

---
+++ exc.py
@@ -0,0 +1,26 @@
+class AttemptsExcededError(Exception):
+    pass
+
+
+class InvalidOtpError(Exception):
+    pass
+
+
+class ProtectedEntityError(Exception):
+    pass
+
+
+class EmptyTableError(Exception):
+    pass
+
+
+class DuplicateError(Exception):
+    pass
+
+
+class EntityNotFoundError(Exception):
+    pass
+
+        
+class IdentificatorError(Exception):
+    pass
