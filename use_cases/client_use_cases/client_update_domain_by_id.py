9657976a8159e21d1d04bb7711e68402f3fa731c
Umohau Haraba <xiricoaviva@gmail.com>
2026-08-01 23:19:41 +0200

    feat(client_use_cases): cria o use case client_update_domain_by_id

---
+++ use_cases/client_use_cases/client_update_domain_by_id.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from Projeto_xirico.repositories.cliente_repository import ClientRepository
+    from Projeto_xirico.profile import Profile
+    from Projeto_xirico.seguranca import Auditoria
+
+
+class ClientUpdateDomainById:
+    def __init__(
+            self,
+            repo: ClientRepository,
+            profile: Profile,
+            audit: Auditoria
+        ):
+        self._repo= repo
+        self._profile= profile
+        self.audit= audit
+
+
+    def execute(self, client_id: int, domain: dict) -> int:
+        effect= self._repo.update(dados= domain, id= client_id)
+        self.audit.auditar(
+            operador= self._profile.id,
+            operacao= 'client_update_domain_by_id',
+            detalhes= f'actualizou o dominio do cliente com id {client_id}'
+        )
+        return effect
