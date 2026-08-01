9cd2e7e10d5654eeb0b49290709717761d86d2a1
Umohau Haraba <xiricoaviva@gmail.com>
2026-08-01 23:38:05 +0200

    feat(client_use_cases): cria o use case search_client_by_id

---
+++ use_cases/client_use_cases/search_client_by_id.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from Projeto_xirico.repositories.cliente_repository import ClientRepository
+    from Projeto_xirico.profile import Profile
+    from Projeto_xirico.seguranca import Auditoria
+
+
+class SearchClientById:
+    def __init__(self,
+        repo: ClientRepository,
+        audit: Auditoria,
+        profile: Profile
+        ):
+        self._repo= repo
+        self._audit= audit
+        self._profile= profile
+
+
+    def execute(self, client_id: int) -> dict:
+        cliente= self._repo.search_by_id(client_id)
+        self._audit.auditar(
+            operador= self._profile.id,
+            operacao= 'search_client_by_id',
+            detalhes= f'pesquisou pelo id {id}'
+        )
+        return cliente
