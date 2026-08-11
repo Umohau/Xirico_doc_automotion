2900bdf4157bdc1e8d1af0ec5ecbba39371330e0
Umohau Haraba <xiricoaviva@gmail.com>
2026-08-11 07:56:46 +0200

    feat(client_use_cases): cria o use case list_client_shipments

---
+++ use_cases/client_use_cases/list_client_shipments.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from Projeto_xirico.repositories.shipment_repository import ShipmentRepository
+
+class ListClientShipments:
+    def __init__(self, repo:ShipmentRepository):
+        self._repo= repo
+
+
+    def execute(self, client_id: str) -> list[dict]:
+        return self._repo.get_shipment_cid(client_id)
