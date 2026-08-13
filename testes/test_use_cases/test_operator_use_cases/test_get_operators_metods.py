9cee27ea8504c65f691d8989445d35af2e9b3d2e
Umohau Haraba <xiricoaviva@gmail.com>
2026-08-13 10:25:23 +0200

    feat(test_get_operators_metods): modifica os testes para cobrirem o retorno de DTOS

--- testes/test_use_cases/test_operator_use_cases/test_get_operators_metods.py
+++ testes/test_use_cases/test_operator_use_cases/test_get_operators_metods.py
@@ -1,5 +1,6 @@
 import pytest
 from unittest.mock import PropertyMock
+from Projeto_xirico.DTOs.operator_DTOs import OperatorGetByAdmResponse, OperatorGetInactiveResponse, OperatorGetResponse
 from Projeto_xirico.use_cases.operator_use_cases.get_inactive_operators import GetInactiveOperators
 from Projeto_xirico.use_cases.operator_use_cases.list_active_operators import ListActiveOperators
 from Projeto_xirico.use_cases.operator_use_cases.search_by_id import SearchByID
@@ -20,7 +21,7 @@ def get_inactive_operators(
 
 
 @pytest.fixture
-def list_active_operators(mock_repo, mock_profile, mock_audit):   
+def list_active_operators(mock_repo, mock_profile, mock_audit):
     return ListActiveOperators(
         repo= mock_repo,
         profile= mock_profile,
@@ -49,42 +50,114 @@ def search_by_name(mock_repo, mock_profile, mock_audit):
     )
 
 
-def test_get_inactive_operator_sucess(get_inactive_operators, mock_repo, mock_audit):
-    get_inactive_operators.execute()
+def test_get_inactive_operator_sucess(
+        get_inactive_operators,
+        mock_repo,
+        mock_audit
+        ):
+    inactives= get_inactive_operators.execute()
     mock_repo.search_inactive.assert_called_once()
     mock_audit.auditar.assert_called_once()
+    assert len(inactives) == 2
+    assert isinstance(inactives[1], OperatorGetInactiveResponse)
 
 
-def test_get_inactive_operators_audit_failled(get_inactive_operators, mock_repo, mock_profile, mock_audit):
-    espected= {"dado": 123}
-    mock_repo.search_inactive.return_value= espected
+
+def test_get_inactive_operators_audit_failled(
+        get_inactive_operators,
+        mock_repo, mock_profile,
+        mock_audit
+        ):
     mock_audit.auditar.side_effect= FileNotFoundError
-    ret= get_inactive_operators.execute()
+    inactives = get_inactive_operators.execute()
     mock_repo.search_inactive.assert_called()
-    assert ret == espected
+    assert len(inactives) == 2
+    assert isinstance(inactives[1],OperatorGetInactiveResponse )
+    
 
 
-def test_list_active_operators_sucess(list_active_operators, mock_repo, mock_profile, mock_audit):
-    list_active_operators.execute()
+def test_list_active_operators_ADM_profile(
+        list_active_operators,
+        mock_repo,
+        mock_profile,
+        mock_audit
+        ):
+    type(mock_profile).ADM= PropertyMock(return_value= True)
+    result= list_active_operators.execute()
     mock_repo.search_all.assert_called_once()
     mock_audit.auditar.assert_called_once()
+    assert len(result) == 2
+    assert isinstance(result[0], OperatorGetByAdmResponse)
+   
 
-
-def test_test_list_active_operators_audit_failled(list_active_operators, mock_repo, mock_profile, mock_audit):
-    mock_audit.auditar.side_effect= FileNotFoundError
-    list_active_operators.execute()
+def test_list_active_operators_non_ADM_profile(
+        list_active_operators,
+        mock_repo, mock_op_dados,
+        mock_profile,
+        mock_audit
+        ):
+    type(mock_profile).ADM= PropertyMock(return_value= False)
+    mock_op_dados['ADM']=False
+    result= list_active_operators.execute()
     mock_repo.search_all.assert_called_once()
+    mock_audit.auditar.assert_called_once()
+    assert len(result) == 2
+    assert isinstance(result[0], OperatorGetResponse)
+    assert result[0].roll == 'operator'
 
 
-def test_search_by_id_sucess(search_by_id, mock_repo, mock_profile, mock_audit):
+def test_test_list_active_operators_audit_failled(
+        list_active_operators,
+        mock_repo,
+        mock_profile,
+        mock_audit
+        ):
+    type(mock_profile).ADM= PropertyMock(return_value= False)
+    mock_audit.auditar.side_effect= FileNotFoundError
+    result= list_active_operators.execute()
+    mock_repo.search_all.assert_called_once()
+    assert isinstance(result[0], OperatorGetResponse)
+
+
+def test_search_by_id_sucess(
+        search_by_id,
+        mock_repo,
+        mock_profile,
+        mock_audit
+        ):
     id=1
-    search_by_id.execute(id)
+    result= search_by_id.execute(id)
     mock_repo.search_id.assert_called_with(id)
     mock_audit.auditar.assert_called_once()
+    assert isinstance(result, OperatorGetByAdmResponse)
 
 
-def test_search_by_name_sucess(search_by_name, mock_repo, mock_profile, mock_audit):
+def test_search_by_name_Adm_profile(
+        search_by_name,
+        mock_repo,
+        mock_profile,
+        mock_audit):
     name='teste'
-    search_by_name.execute(name)
+    type(mock_profile).ADM= PropertyMock(return_value= True)
+    result= search_by_name.execute(name)
     mock_repo.search_name.assert_called_with(name)
     mock_audit.auditar.assert_called()
+    assert isinstance(result[0], OperatorGetByAdmResponse)
+
+
+def test_search_by_name_non_adm_profile(
+        search_by_name,
+        mock_repo,
+        mock_profile,
+        mock_audit,
+        caplog
+        ):
+    name= 'teste'
+    type(mock_profile).ADM= PropertyMock(return_value= False)
+    result= search_by_name.execute(name)
+    logs= caplog.records
+    mock_repo.search_name.assert_called_with(name)
+    mock_audit.auditar.assert_called()
+    assert len(result)==2
+    assert isinstance(result[0], OperatorGetResponse)
+   
